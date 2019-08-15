607ffe838fd7fa470da6a48392c82c36bb1670ac
Bogdan <krabog@gmail.com>
2019-08-15 15:37:39 +0300

    add reg form.ftl and js

--- src/main/java/ua/registration_form/entitry/User.java
+++ src/main/java/ua/registration_form/entitry/User.java
@@ -1,4 +1,12 @@
 package ua.registration_form.entitry;
 
+import lombok.Data;
+
+@Data
 public class User {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
 }
